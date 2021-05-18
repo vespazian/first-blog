bcb5dd572da5a0fcdad5c5715220b14f68042e4b
= <=>
2021-05-18 22:21:21 +0300

    models add models folder

--- markdown-blog/server.js
+++ markdown-blog/server.js
@@ -1,24 +1,34 @@
 //console.log('ceva?');
 
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 const personName = 'Georgel'
 
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true , useUnifiedTopology: true })
+
 app.set ('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
-
-
+app.use(express.urlencoded({ extended: false}))
 
 app.get('/', (req, res) =>{   
     const articles = [{
         title: 'Test Article',
-        createdAt: Date.now(),
-        description: 'Test description'
+        createdAt: new Date(),
+        description: `at View.render (E:\\DigitalExplorer\\EXPLORER\\JustAWebServer\\markdown-blog\\node_modules\\express\\lib\\view.js:135:8
+            dddddddddddddddddddddddddd 
+            asdasdasd`
+    },
+    {
+        title: 'Test Seccond Article',
+        createdAt: new Date(),
+        description: 'Test description for article two'
     }]
     //res.render('index', { text: 'Hello ' + personName + '!'}) //views//index.ejs
-    res.render('index', { articles: articles})
+    res.render('articles/index', { articles: articles})
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
